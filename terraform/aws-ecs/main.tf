d8ee23fe1e3f9da16fd928f602f1682ea4bc5a2a
Caio Amaral <caiobrandao.doamaral@wirecard.com>
2019-05-17 11:41:22 +0200

    Run task using awscli

--- terraform/aws-ecs/main.tf
+++ terraform/aws-ecs/main.tf
@@ -3,12 +3,16 @@ provider "aws" {
   profile = "dbmigration"
 }
 
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = "us-east-1a"
+}
+
 resource "aws_ecs_cluster" "db_deployment" {
   name = "db-deployment-cluster"
 }
 
 resource "aws_ecs_task_definition" "db_deployment" {
-  container_definitions = "${file("./db-migration-task-definition.json")}"
+  container_definitions = "${file("./db-migration-container-definitions.json")}"
   family = "db-deployment-definition"
   cpu = "256"
   memory = "512"
@@ -16,6 +20,16 @@ resource "aws_ecs_task_definition" "db_deployment" {
   requires_compatibilities = [
     "FARGATE"
   ]
+
+  execution_role_arn = "arn:aws:iam::114795752263:role/ecsTaskExecutionRole"
+
+  depends_on = [
+    "aws_ecs_cluster.db_deployment"
+  ]
+
+  //  provisioner "local-exec" {
+  //    command = "aws ecs run-task --cluster ${aws_ecs_cluster.db_deployment.arn} --task-definition ${aws_ecs_task_definition.db_deployment.arn} --network-configuration 'awsvpcConfiguration={subnets=[${aws_default_subnet.default_az1.id}],assignPublicIp=ENABLED}' --launch-type=FARGATE --profile dbmigration --region us-east-1"
+  // }
 }
 
 output "cluster_arn" {
@@ -24,4 +38,8 @@ output "cluster_arn" {
 
 output "task_definition_arn" {
   value = "${aws_ecs_task_definition.db_deployment.arn}"
+}
+
+output "default_subnet_id" {
+  value = "${aws_default_subnet.default_az1.id}"
 }
